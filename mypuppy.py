7baf370656d5206897921389db9142f41b3f71b4
David Antonio Toro Medina <david-toro92@hotmail.com>
2020-05-08 23:41:47 -0500

    v3

---
+++ mypuppy.py
@@ -0,0 +1,8 @@
+import cv2
+img = cv2.imread('DATA/00-puppy.jpg')
+while True:
+    cv2.imshow('Puppy',img)
+    # IF we've waited at Least I ms AND we've pressed the Esc
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
---
+++ myscript.py
@@ -0,0 +1,1 @@
+print(" I am running myscript.py")
